f81b0f7284597f658157e50c54dbd7bd12f16dde
Andrey Ryabtsev <kodacci@gmail.com>
2024-11-16 22:46:57 +0300

    fix: #27 fixed docker entrypoint

--- distrib/deploy/entrypoint.sh
+++ distrib/deploy/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 CONFIG_PATH=/application/application.yaml
 echo "Configuration file path: $CONFIG_PATH}"
-while [ ! -e CONFIG_PATH ]
+while [ ! -e $CONFIG_PATH ]
 do
   echo "Waiting for configuration file $CONFIG_PATH"
   sleep 1
