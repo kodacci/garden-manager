5c643a8790d4ddc7b8aef015b99bdafd0c6437d6
Andrey Ryabtsev <kodacci@gmail.com>
2024-11-17 17:58:08 +0300

    feat: #27 using approle for vault auth

--- distrib/deploy/entrypoint.sh
+++ distrib/deploy/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONFIG_PATH=/application/application.yaml
-echo "Configuration file path: $CONFIG_PATH}"
+echo "Configuration file path: $CONFIG_PATH"
 while [ ! -e $CONFIG_PATH ]
 do
   echo "Waiting for configuration file $CONFIG_PATH"
