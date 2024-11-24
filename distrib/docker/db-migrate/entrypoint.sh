5f69cafb6d227a6fb1b2fa4f705f538c74f2a13d
Andrey Ryabtsev <kodacci@gmail.com>
2024-11-24 20:17:35 +0300

    feat: #40 adds istio-proxy quit command to finish db-migrate job

--- distrib/docker/db-migrate/entrypoint.sh
+++ distrib/docker/db-migrate/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 PROPS_FILE_PATH=/config/liquibase/liquibase.properties
 echo "Liquibase properties file path: $PROPS_FILE_PATH"
 while [ ! -e $PROPS_FILE_PATH ]
@@ -12,3 +14,6 @@ echo "Got liquibase props. Running liquibase ..."
 echo ""
 
 liquibase --defaults-file $PROPS_FILE_PATH --changelog-file db/changelog-root.yaml update
+
+echo "Stopping istio-proxy sidecar to end job execution"
+curl -X POST http://localhost:15020/quitquitquit
