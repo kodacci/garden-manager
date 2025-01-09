8021adfa1346b44148e55d8dee5e516d54e4d461
Andrey Ryabtsev <kodacci@gmail.com>
2025-01-09 11:32:14 +0300

    feat: #56 fixed controller update method

--- core/src/main/java/ru/ra_tech/garden_manager/core/controllers/gardens/GardensController.java
+++ core/src/main/java/ru/ra_tech/garden_manager/core/controllers/gardens/GardensController.java
@@ -3,17 +3,21 @@ import io.micrometer.core.annotation.Timed;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.ra_tech.garden_manager.core.controllers.AbstractController;
 import ru.ra_tech.garden_manager.core.controllers.gardens.dto.CreateGardenRequest;
 import ru.ra_tech.garden_manager.core.services.api.GardenService;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,8 +28,8 @@ public class GardensController extends AbstractController implements GardensApi 
     @Override
     @PostMapping(
             value = "",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE
     )
     @Timed("gardens.create")
     public ResponseEntity<Object> createGarden(CreateGardenRequest request) {
@@ -33,21 +37,21 @@ public class GardensController extends AbstractController implements GardensApi 
     }
 
     @Override
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     @Timed("gardens.find")
     public ResponseEntity<Object> findGarden(@PathVariable Long id) {
         return toResponse(getUserId().flatMap(userId -> service.findGarden(id, userId)));
     }
 
     @Override
-    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "", produces = APPLICATION_JSON_VALUE)
     @Timed("gardens.list")
     public ResponseEntity<Object> listGardens() {
         return toResponse(getUserId().flatMap(service::listGardens));
     }
 
     @Override
-    @PostMapping(value = "/{id}/add_participant/{participantId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/{id}/add_participant/{participantId}", produces = APPLICATION_JSON_VALUE)
     @Timed("gardens.add-user")
     public ResponseEntity<Object> addParticipant(@PathVariable Long id, @PathVariable Long participantId) {
         return toResponse(getUserId().flatMap(userId -> service.addParticipant(id, participantId, userId)));
@@ -56,7 +60,7 @@ public class GardensController extends AbstractController implements GardensApi 
     @Override
     @DeleteMapping(
             value = "/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE}
+            produces = {APPLICATION_JSON_VALUE, APPLICATION_PROBLEM_JSON_VALUE}
     )
     @Timed("gardens.delete")
     public ResponseEntity<Object> deleteGarden(@Positive @PathVariable Long id) {
@@ -64,7 +68,12 @@ public class GardensController extends AbstractController implements GardensApi 
     }
 
     @Override
-    public ResponseEntity<Object> updateGarden(Long id, CreateGardenRequest update) {
+    @PutMapping(
+            value = "/{id}",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = {APPLICATION_JSON_VALUE, APPLICATION_PROBLEM_JSON_VALUE}
+    )
+    public ResponseEntity<Object> updateGarden(@Positive @PathVariable Long id, @RequestBody CreateGardenRequest update) {
         return toResponse(getUserId().flatMap(userId -> service.updateGarden(id, userId, update)));
     }
 }
