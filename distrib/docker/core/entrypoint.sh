c8ec4abb06b77e38558529d4e49236f92ca231c0
Andrey <kodacci@gmail.com>
2025-02-06 23:04:44 +0300

    Feature/61 deploy with env (#68)
    closes #61

--- distrib/docker/core/entrypoint.sh
+++ distrib/docker/core/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CONFIG_PATH=/application/application.yaml
-echo "Configuration file path: $CONFIG_PATH"
-while [ ! -e $CONFIG_PATH ]
+ENV_PATH=/application/.env
+echo "Configuration file path: $ENV_PATH"
+while [ ! -e $ENV_PATH ]
 do
-  echo "Waiting for configuration file $CONFIG_PATH"
+  echo "Waiting application environment $ENV_PATH"
   sleep 1
 done
 
-echo "Configuration file found, executing main app ..."
+echo "Environment file found, starting main app ..."
 
-java -jar /home/garden-manager/garden-manager-core.jar --spring.config.location=$CONFIG_PATH
+env -S "$(cat $ENV_PATH)" java -jar /home/garden-manager/garden-manager-core.jar
